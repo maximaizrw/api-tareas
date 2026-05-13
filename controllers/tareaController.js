b247fbea53cb993807aea1131f1795eb13a2d6ec
maximaizrw <maximaizrw@gmail.com>
2026-05-13 11:32:47 -0300

    Configuracion inicial

---
+++ controllers/tareaController.js
@@ -0,0 +1,5 @@
+const tareas = [
+  { id: 1, titulo: "Estudiar Express" },
+  { id: 2, titulo: "Hacer TP" },
+  { id: 3, titulo: "Subir proyecto a GitHub" }
+];
---
+++ routes/tarea.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const tareaController = require('../controllers/tareaController');
+
+// Rutas para tareas
+
+module.exports = router;

e1a2aa36ed37c256b3f0a279051a57cef7e80e96
chalos67 <chalosampini@hotmail.com>
2026-06-22 20:15:01 -0300

    Prepara aplicación para pruebas automatizadas

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const app = express();
+
 const tareaRoutes = require('./routes/tarea');
 
 app.use(express.json());
-app.use('/api', tareaRoutes);
 
-const PORT = 3000;
+app.use('/tareas', tareaRoutes);
 
+module.exports = app;
 
-const tarea = require('./routes/tarea');
-app.use(express.json());
-app.use('/tareas', tarea);
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
 
 
